89679e012f3b98e39f87e6b48affa29c0977e346
Ochagwu Paul <ochagwupaul@yahoo.com>
2022-07-16 11:41:54 +0100

    change my database configuraton

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const Pool = require("pg").Pool;
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}/${process.env.PG_DATABASE}`;
+const devConfig = {
+  connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}/${process.env.PG_DATABASE}`,
+};
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
 };
 
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-});
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 module.exports = pool;
