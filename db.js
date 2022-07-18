9b7e692bde4c36043df962eb93c05986bfd501cf
Ochagwu Paul <ochagwupaul@yahoo.com>
2022-07-18 09:45:23 +0100

    changed my database configuration to use heroku database-url

--- db.js
+++ db.js
@@ -3,14 +3,11 @@ const Pool = require("pg").Pool;
 const devConfig = {
   connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}/${process.env.PG_DATABASE}`,
 };
-let pool;
+
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.NODE_ENV === "production") {
-  pool = new Pool(proConfig);
-} else {
-  pool = new Pool(devConfig);
-}
+const pool = new Pool(proConfig);
+
 module.exports = pool;
